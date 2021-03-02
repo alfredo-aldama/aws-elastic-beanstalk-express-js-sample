1a51de45034e35fbef17368c055ab0f069136c65
Alfredo Aldama <alfredo_aldama@hotmail.com>
2021-03-02 12:17:55 -0600

    Full pipeline test 20210302 1216 esta dsc es de la del commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! this is an exercise for AO to learn DEvOps with AWS'));
+app.get('/', (req, res) => res.send('Hello World! this is an exercise for AO to learn DEvOps with AWS, this is a new Modify 20210302 1216'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
